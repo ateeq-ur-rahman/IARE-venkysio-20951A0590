da4d9b831f71f4782c84f719c6883a5b159a7c23
ateeq-ur-rahman <93069335+ateeq-ur-rahman@users.noreply.github.com>
2023-03-10 06:07:34 +0530

    Add files via upload

---
+++ heapsort2.py
@@ -0,0 +1,26 @@
+def heapify(a,n,i):
+    lar=i
+    l=2*i+1
+    r=2*i+2
+
+    if l<n and a[i]<a[l]:
+        lar=l
+    if r<n and a[l]<a[r]:
+        lar=r
+    
+    if lar!=i:
+        a[i],a[lar]=a[lar],a[i]
+        heapify(a,n,lar)
+
+def heapsort(a):
+    n=len(a)
+    for i in range(n//2-1,-1,-1):
+        heapify(a,n,i)
+    for i in range(n-1,0,-1):
+        a[i],a[0]=a[0],a[i]
+        heapify(a,i,0)
+a=list(map(int,input().split()))
+heapsort(a)
+n=len(a)
+for i in range(n):
+    print(a[i])
